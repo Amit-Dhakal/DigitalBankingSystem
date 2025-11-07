8047483c2fc70b0a90eeff061fb1b65c2a15046e
Amit Dhakal <amit.dhakal423@gmail.com>
2025-11-07 14:20:30 +0545

    Initial Setup

---
+++ src/test/java/com/system/DigitalBankingApplication/DigitalBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.DigitalBankingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
